39d1d68743115e4c66fc7ab7338c0f0804c1256a
Gustavo Santos <estudosgusta32@gmail.com>
2026-01-29 12:27:20 -0300

    Update carregarAlbum.js

--- assets/js/carregarAlbum.js
+++ assets/js/carregarAlbum.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             timeoutToken = setTimeout(() => {
                 buscarMusica(termo);
-            }, 00);
+            }, 1000);
         });
     }
 
